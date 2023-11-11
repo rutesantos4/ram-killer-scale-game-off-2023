8a8713ec0e4f0b461114eb07cb061fd0223f5e6c
RuteSantos <1160663@isep.ipp.pt>
2023-11-11 19:59:14 +0000

    feat: add selection menu (choose operating system, browser to fight and map)

---
+++ selection_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func _on_operating_system_mascot_pressed():
+	pass # Replace with function body.
+
+func _on_browser_to_fight_pressed():
+	pass # Replace with function body.
+
+func _on_map_pressed():
+	pass # Replace with function body.
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://main_menu.tscn")
