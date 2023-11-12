44d08ccf6f7e14fa204c2e19107b35e3a8010513
RuteSantos <1160663@isep.ipp.pt>
2023-11-12 22:13:29 +0000

    feat: set the game info in the selection menu when the 'Play' button is pressed

--- selection_menu.gd
+++ selection_menu.gd
@@ -1,8 +1,20 @@
 extends Control
 
+@export var option_button_operating_system_path : NodePath
+@onready var option_button_operating_system = get_node(option_button_operating_system_path)
+
+@export var option_button_browser_to_fight_path : NodePath
+@onready var option_button_browser_to_fight = get_node(option_button_browser_to_fight_path)
+
+@export var option_button_map_path : NodePath
+@onready var option_button_map = get_node(option_button_map_path)
+
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://main_menu.tscn")
 
 func _on_play_pressed():
-	print("Play!")
+	var operating_system_icon = option_button_operating_system.get_item_icon(option_button_operating_system.get_selected_id())
+	var browser_to_fight_icon = option_button_browser_to_fight.get_item_icon(option_button_browser_to_fight.get_selected_id())
+	var map_icon = option_button_map.get_item_icon(option_button_map.get_selected_id())
+	SceneSwitcher.change_scene("res://game.tscn", GameInfo.new(browser_to_fight_icon, operating_system_icon, map_icon))
 
