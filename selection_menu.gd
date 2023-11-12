edbacc68831a19369362d488947341d8a2e42111
RuteSantos <1160663@isep.ipp.pt>
2023-11-12 16:44:46 +0000

    feat: add play button in selection menu

--- selection_menu.gd
+++ selection_menu.gd
@@ -1,14 +1,8 @@
 extends Control
 
-
-func _on_operating_system_mascot_pressed():
-	pass # Replace with function body.
-
-func _on_browser_to_fight_pressed():
-	pass # Replace with function body.
-
-func _on_map_pressed():
-	pass # Replace with function body.
-
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://main_menu.tscn")
+
+func _on_play_pressed():
+	print("Play!")
+
