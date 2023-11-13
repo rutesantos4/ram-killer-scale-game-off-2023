1400c999ee8973c978b40bf15cfe43a0592a5670
RuteSantos <1160663@isep.ipp.pt>
2023-11-13 20:42:48 +0000

    fix: velocity of player

--- player.gd
+++ player.gd
@@ -1,25 +1,16 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const STOP_DISTANCE = 100.0
+const MAX_SPEED = 250.0
+const MIN_SPEED = 50.0
 
 func _physics_process(delta):
 	var target : Vector2 = get_global_mouse_position()
-	var input_direction : Vector2 = position.direction_to(target)
-	velocity = input_direction * SPEED
 	var input_distance : float = position.distance_to(target)
-	if input_distance > STOP_DISTANCE:
-		move_and_slide()
-	else:
-		print(input_direction)
-		if(velocity.x < 0):
-			velocity = Vector2(velocity.x - STOP_DISTANCE, velocity.y).normalized() * SPEED
-		else:
-			velocity = Vector2(velocity.x + STOP_DISTANCE, velocity.y).normalized() * SPEED
-		if(velocity.y < 0):
-			velocity = Vector2(velocity.x, velocity.y - STOP_DISTANCE).normalized() * SPEED
-		else:
-			velocity = Vector2(velocity.x, velocity.y + STOP_DISTANCE).normalized() * SPEED
-		move_and_slide()
-		
+	var input_direction : Vector2 = position.direction_to(target)
+	
+	velocity = input_direction * MAX_SPEED
+	velocity = velocity.normalized() * max(MIN_SPEED, min(MAX_SPEED, input_distance))
+	
+	move_and_slide()
+
 
