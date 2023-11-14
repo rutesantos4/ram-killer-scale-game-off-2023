5f61f5cadc6f45fb4cbea20945e4e6d7fc2d655a
RuteSantos <1160663@isep.ipp.pt>
2023-11-14 20:39:30 +0000

    feat: set the player image as the one selected

--- player.gd
+++ player.gd
@@ -3,6 +3,14 @@ extends CharacterBody2D
 const MAX_SPEED = 250.0
 const MIN_SPEED = 50.0
 
+func _ready():
+	# Set the player image
+	$PlayerSprite2D.texture = SceneSwitcher.get_game_info().get_player_image()
+	# Set the player position
+	$PlayerSprite2D.position.x = 0
+	$PlayerSprite2D.position.y = 0
+
+
 func _physics_process(delta):
 	var target : Vector2 = get_global_mouse_position()
 	var input_distance : float = position.distance_to(target)
@@ -12,5 +20,3 @@ func _physics_process(delta):
 	velocity = velocity.normalized() * max(MIN_SPEED, min(MAX_SPEED, input_distance))
 	
 	move_and_slide()
-
-
