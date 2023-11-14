3ac6e841a77bf905d3150f0fe4b9e9aed56e01ec
freitzzz <1160907@isep.ipp.pt>
2023-11-14 20:10:16 +0000

    refactor(dash): multiply speed instead of direction

--- player.gd
+++ player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 const MAX_SPEED = 250.0
 const MIN_SPEED = 50.0
-const DASH_DISTANCE_MULTIPLIER = 3.0
+const DASH_SPEED_MULTIPLIER = 4.5
 
 func _shouldDash():
 	return Input.is_key_pressed(KEY_SHIFT)
@@ -11,11 +11,12 @@ func _physics_process(delta):
 	var target : Vector2 = get_global_mouse_position()
 	var input_distance : float = position.distance_to(target)
 	var input_direction : Vector2 = position.direction_to(target)
+	var speed = MAX_SPEED
 	
 	if(_shouldDash()):
-		input_distance *= DASH_DISTANCE_MULTIPLIER
+		speed *= DASH_SPEED_MULTIPLIER
 	
-	velocity = input_direction * MAX_SPEED
-	velocity = velocity.normalized() * max(MIN_SPEED, max(MAX_SPEED, input_distance))
+	velocity = input_direction * speed
+	velocity = velocity.normalized() * max(MIN_SPEED, max(speed, input_distance))
 		
 	move_and_slide()
