84afa3baa4845bccf0423f2a3bd8958ff1af6cd4
RuteSantos <1160663@isep.ipp.pt>
2023-11-14 20:40:28 +0000

    feat: set dynamically the items of the option buttons in the selection menu

--- selection_menu.gd
+++ selection_menu.gd
@@ -9,12 +9,32 @@ extends Control
 @export var option_button_map_path : NodePath
 @onready var option_button_map = get_node(option_button_map_path)
 
+func _ready():
+	var enemies_size = GameAssetsFactory.Enemies.size()
+	for i in range(0, enemies_size):
+		var enemy = GameAssetsFactory.Enemies[i]
+		option_button_browser_to_fight.add_icon_item(enemy.get_icon_image(), enemy.get_name(), i)
+		
+	var players_size = GameAssetsFactory.Players.size()
+	for i in range(0, players_size):
+		var player = GameAssetsFactory.Players[i]
+		option_button_operating_system.add_icon_item(player.get_icon_image(), player.get_name(), i)
+		
+	var maps_size = GameAssetsFactory.Maps.size()
+	for i in range(0, maps_size):
+		var map = GameAssetsFactory.Maps[i]
+		option_button_map.add_icon_item(map.get_icon_image(), map.get_name(), i)
+	
+
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://main_menu.tscn")
 
 func _on_play_pressed():
-	var operating_system_icon = option_button_operating_system.get_item_icon(option_button_operating_system.get_selected_id())
-	var browser_to_fight_icon = option_button_browser_to_fight.get_item_icon(option_button_browser_to_fight.get_selected_id())
-	var map_icon = option_button_map.get_item_icon(option_button_map.get_selected_id())
-	SceneSwitcher.change_scene("res://game.tscn", GameInfo.new(browser_to_fight_icon, operating_system_icon, map_icon))
-
+	var player_selected = option_button_operating_system.get_selected_id();
+	var enemy_selected = option_button_browser_to_fight.get_selected_id();
+	var map_selected = option_button_map.get_selected_id();
+	
+	SceneSwitcher.change_scene("res://game.tscn", GameInfo.new(
+		GameAssetsFactory.Players[player_selected].get_full_size_image(),
+		GameAssetsFactory.Enemies[enemy_selected].get_full_size_image(),
+		GameAssetsFactory.Maps[map_selected].get_full_size_image()))
