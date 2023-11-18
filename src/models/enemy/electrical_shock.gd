9239f05017315d8ed5e1c72e1a296b30e6ae2088
freitzzz <1160907@isep.ipp.pt>
2023-11-18 22:25:01 +0000

    feat: add target position to electrical shock

--- src/models/enemy/electrical_shock.gd
+++ src/models/enemy/electrical_shock.gd
@@ -2,8 +2,13 @@ extends PositionalEntity
 
 class_name ElectricalShock
 
+var _target_position: Vector2
+
 func _init(
 	skin: SkinAsset,
-	initial_position: Vector2
+	initial_position: Vector2,
+	target_position: Vector2
 ):
 	super(skin, initial_position)
+	
+	_target_position = target_position
