20a330424383e48d36aeededd62558494c92908b
freitzzz <1160907@isep.ipp.pt>
2023-11-18 21:44:11 +0000

    feat: implement spawn tabs

--- src/models/enemy/browser.gd
+++ src/models/enemy/browser.gd
@@ -10,3 +10,22 @@ func _init(
 func fire():
 	#TODO: implement
 	pass
+
+func spawn():
+	var tabs_to_spawn = randi_range(0, 6)
+	var maximum_tabs_joint_memory_size = memory_size / 2
+	var tabs = []
+	
+	for i in tabs_to_spawn:
+		var tab_memory_size = maximum_tabs_joint_memory_size / tabs_to_spawn
+		
+		tabs.push_front(
+			Tab.new(
+				_skin,
+				tab_memory_size
+			)
+		)
+		
+		memory_size -= tab_memory_size
+	
+	return tabs
