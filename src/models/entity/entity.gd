0807fc5268dbaaeecded410fb1ab31316398702b
freitzzz <1160907@isep.ipp.pt>
2023-11-18 22:04:50 +0000

    feat: abstract skin and position of game entities

---
+++ src/models/entity/entity.gd
@@ -0,0 +1,10 @@
+extends Object
+
+class_name Entity
+
+var _skin: SkinAsset
+
+func _init(
+	skin: SkinAsset
+):
+	_skin = skin
---
+++ src/models/entity/positional_entity.gd
@@ -0,0 +1,12 @@
+extends Entity
+
+class_name PositionalEntity
+
+var position: Vector2
+
+func _init(
+	skin: SkinAsset,
+	initial_position: Vector2
+):
+	super(skin)
+	position = initial_position
