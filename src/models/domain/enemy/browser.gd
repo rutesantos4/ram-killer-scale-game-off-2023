e9382791284927324501038e93c933f9243ac95f
RuteSantos <1160663@isep.ipp.pt>
2023-11-19 19:46:03 +0000

    fix: generate tab skin

--- src/models/domain/enemy/browser.gd
+++ src/models/domain/enemy/browser.gd
@@ -21,11 +21,11 @@ func fire(target_position: Vector2):
 		target_position
 	)
 
-func spawn():
+func spawn() -> Array[Tab]:
 	randomize()
 	var tabs_to_spawn = randi_range(0, 6)
 	var maximum_tabs_joint_memory_size = memory_size / 2
-	var tabs = []
+	var tabs: Array[Tab] = []
 	
 	for i in tabs_to_spawn:
 		var tab_memory_size = maximum_tabs_joint_memory_size / tabs_to_spawn
@@ -44,4 +44,5 @@ func spawn():
 func generate_tab_skin():
 	var tabs = GameAssetsFactory.Tabs
 	var index = randi_range(0, tabs.size() - 1)
-	return tabs[index]
+	var tab = tabs[index]
+	return SkinAsset.new(tab.get_name(), tab.s())
