8a82b0bd8a0c5fe69d7baf56807e06100b9f970d
RuteSantos <1160663@isep.ipp.pt>
2023-11-21 22:35:44 +0000

    feat: implemented the body_entered signal to when the player attack enters a Node2D

--- src/nodes/player_attack.gd
+++ src/nodes/player_attack.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@export var tab: Node2D
+
 const SPEED: float = 25.0
 
 func _ready():
@@ -10,7 +12,14 @@ func _ready():
 	var new_shape = CircleShape2D.new()
 	new_shape.radius = max(enemy_size.x, enemy_size.y) / 2
 	$CollisionShape2D.shape = new_shape
+	
 
 func _physics_process(delta):
 	position += transform.x * SPEED
-	
+
+func _on_body_entered(body: Node2D):
+	print(body.name)
+	# TODO: fix this - not all tabs are marked as 'Tab' (some are CharacterBody2D)
+	if(body.name == 'Tab'):
+		print("Here")
+		body.got_attacked()
