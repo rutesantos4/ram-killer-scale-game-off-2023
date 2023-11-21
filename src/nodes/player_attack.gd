914767058376df6038d27b93129ebe216257c83e
RuteSantos <1160663@isep.ipp.pt>
2023-11-21 21:56:35 +0000

    feat: implement the movement of player attack

--- src/nodes/player_attack.gd
+++ src/nodes/player_attack.gd
@@ -1,4 +1,4 @@
-extends RigidBody2D
+extends Area2D
 
 const SPEED: float = 25.0
 
@@ -10,6 +10,7 @@ func _ready():
 	var new_shape = CircleShape2D.new()
 	new_shape.radius = max(enemy_size.x, enemy_size.y) / 2
 	$CollisionShape2D.shape = new_shape
+
+func _physics_process(delta):
+	position += transform.x * SPEED
 	
-#func _physics_process(delta):
-#	position += transform.x * SPEED
