733d6b20e68676fb4b7786570a0c7a6e71c81beb
RuteSantos <1160663@isep.ipp.pt>
2023-11-21 18:59:40 +0000

    feat: add player attack scene

---
+++ src/nodes/player_attack.gd
@@ -0,0 +1,15 @@
+extends RigidBody2D
+
+const SPEED: float = 25.0
+
+func _ready():
+	var texture = GameAssetsFactory.PlayerAttack.xs()
+	$Sprite2D.texture = texture
+	
+	var enemy_size : Vector2 = texture.get_size()
+	var new_shape = CircleShape2D.new()
+	new_shape.radius = max(enemy_size.x, enemy_size.y) / 2
+	$CollisionShape2D.shape = new_shape
+	
+#func _physics_process(delta):
+#	position += transform.x * SPEED
