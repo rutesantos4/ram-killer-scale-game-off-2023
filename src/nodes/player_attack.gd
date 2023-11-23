9795e99cef25e617a032d5800a503ec165814c47
RuteSantos <1160663@isep.ipp.pt>
2023-11-23 22:11:56 +0000

    feat: decrease ram when tab is closed

--- src/nodes/player_attack.gd
+++ src/nodes/player_attack.gd
@@ -5,13 +5,15 @@ extends Area2D
 const SPEED: float = 25.0
 const MAX_DISTANCE: float = 500.0
 
-var player: Player
 var game_node: Node
+var player: Player
+var ram: RAM
 var initial_position: Vector2
 
 func _ready():
 	game_node = get_tree().root.get_node("/root/Game")
 	player = SceneSwitcher.get_game_state().player
+	ram = SceneSwitcher.get_game_state().ram
 	initial_position = position
 	
 	var texture = GameAssetsFactory.PlayerAttack.xs()
@@ -34,8 +36,10 @@ func _on_body_entered(body: Node2D):
 	if body is TabScene:
 		var tab_scene = body as TabScene
 		tab_scene.got_attacked()
-		player.close(tab_scene.enemy)
+		var tab: Tab = tab_scene.enemy
+		player.close(tab)
 		game_node.player_points_updated.emit()
+		ram.decrease_from_tab_close(tab)
 	delete()
 
 func delete():
