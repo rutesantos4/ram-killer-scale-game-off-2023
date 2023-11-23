f670eef3a3a5a53379eb592b3c44637e81cb4b82
freitzzz <1160907@isep.ipp.pt>
2023-11-23 20:26:54 +0000

    feat: pause and resume game on pause button pressed + escape key clicked

--- src/nodes/pause_button.gd
+++ src/nodes/pause_button.gd
@@ -1,10 +1,37 @@
 extends TextureButton
 
+var game_node: Node
 var pause_menu: Control
+var pausable_group: SceneTree
+var game: Game
 
 func _ready():
-	pause_menu = get_parent().get_node("PauseMenu")
-	self.connect("pressed", _on_game_paused)	
+	game_node = get_tree().current_scene
+	
+	pausable_group = game_node.get_node("Pausable")
+	pause_menu = game_node.get_node("Stickable/PauseMenu")
+	game = SceneSwitcher.get_game_state()
+	
+	self.connect("pressed", _on_game_paused)
+	
+func _process(delta):
+	if Input.is_action_just_pressed("Pause"):
+		_on_game_paused()
 
 func _on_game_paused():
-	pause_menu.visible = !pause_menu.visible
+	if(game.is_paused()):
+		game.resume()
+	else:
+		game.pause()
+
+	game_node.game_status_updated.emit() 
+
+func _on_game_status_updated():
+	pause_menu.visible = is_game_paused
+	
+	if(game.is_paused()):
+		self.texture_normal = GameAssetsFactory.Play.s()
+		pausable_group.process_mode = Node.PROCESS_MODE_DISABLED
+	else:
+		self.texture_normal = GameAssetsFactory.Pause.s()
+		pausable_group.process_mode = Node.PROCESS_MODE_INHERIT
