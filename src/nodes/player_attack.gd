5f68539d69d09f178d13195348b5943dd834b140
RuteSantos <1160663@isep.ipp.pt>
2023-11-23 22:12:33 +0000

    feat: update browser memory and scale when tab is closed

--- src/nodes/player_attack.gd
+++ src/nodes/player_attack.gd
@@ -8,12 +8,16 @@ const MAX_DISTANCE: float = 500.0
 var game_node: Node
 var player: Player
 var ram: RAM
+var browser: Browser
+var browser_scene: Node
 var initial_position: Vector2
 
 func _ready():
 	game_node = get_tree().root.get_node("/root/Game")
 	player = SceneSwitcher.get_game_state().player
 	ram = SceneSwitcher.get_game_state().ram
+	browser = SceneSwitcher.get_game_state().enemy
+	browser_scene = game_node.get_node("Browser")
 	initial_position = position
 	
 	var texture = GameAssetsFactory.PlayerAttack.xs()
@@ -23,7 +27,6 @@ func _ready():
 	var new_shape = CircleShape2D.new()
 	new_shape.radius = max(enemy_size.x, enemy_size.y) / 2
 	$CollisionShape2D.shape = new_shape
-	
 
 func _physics_process(delta):
 	var next_position = position + transform.x * SPEED
@@ -40,6 +43,9 @@ func _on_body_entered(body: Node2D):
 		player.close(tab)
 		game_node.player_points_updated.emit()
 		ram.decrease_from_tab_close(tab)
+		browser.tab_closed(tab)
+		browser_scene.scale = browser.get_scale(browser_scene.scale)
+		game_node.ram_updated.emit()
 	delete()
 
 func delete():
