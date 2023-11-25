5518d3f2a8a2905ac88410a02f3dccadd1d0adb6
RuteSantos <1160663@isep.ipp.pt>
2023-11-25 19:38:51 +0000

    feat: generate tab in a specific circle area

---
+++ src/ai/tab_spawner.gd
@@ -0,0 +1,43 @@
+extends Object
+
+class_name TabSpawner
+
+const TAB_MEMORY_SIZE = 10
+const MAX_RADIUS_SPAWN_TAB = 200
+
+static func generate(browser: Browser, maximum_tabs_joint_memory_size: int) -> Array[Tab]:
+	randomize()
+	var tabs: Array[Tab] = []
+	var max_tabs_to_spawn = maximum_tabs_joint_memory_size / TAB_MEMORY_SIZE
+	var tabs_to_spawn = randi_range(1, min(max_tabs_to_spawn, 6))
+	
+	for i in tabs_to_spawn:
+		var tab = Tab.new(
+				generate_tab_skin(),
+				calculate_tab_position(browser.position),
+				TAB_MEMORY_SIZE,
+				browser
+			)
+		tabs.push_front(tab)
+		browser.spawn_tabs.push_front(tab)
+		browser.memory_size += TAB_MEMORY_SIZE
+	print(" ")
+	
+	return tabs
+
+
+static func generate_tab_skin():
+		var tabs = GameAssetsFactory.Tabs
+		var index = randi_range(0, tabs.size() - 1)
+		var tab = tabs[index]
+		return SkinAsset.new(tab.get_name(), tab.s())
+
+
+static func calculate_tab_position(center: Vector2) -> Vector2:
+	randomize()
+	var angle = randf_range(0, PI * 2)
+	var radius = randf_range(1, MAX_RADIUS_SPAWN_TAB)
+	print("angle ", angle, " radius ", radius)
+	var direction = Vector2(cos(angle), sin(angle))
+	
+	return center + direction * radius
