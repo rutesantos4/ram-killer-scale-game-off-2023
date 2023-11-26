7967bda01bf1f2bbd19119e400991aa4983471cb
freitzzz <1160907@isep.ipp.pt>
2023-11-26 12:24:05 +0000

    feat: play click sfx on button click

--- src/nodes/theme/textured_button.gd
+++ src/nodes/theme/textured_button.gd
@@ -3,6 +3,10 @@ class_name TexturedButton
 
 func _ready():
 	self.connect("mouse_entered", _on_hover)
+	self.connect("pressed", _on_click)
 
 func _on_hover():
 	AudioScene.play_sfx(GameAssetsFactory.HoverSFX)
+
+func _on_click():
+	AudioScene.play_sfx(GameAssetsFactory.ClickSFX)
