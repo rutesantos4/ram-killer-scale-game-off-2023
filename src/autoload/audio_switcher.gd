d6c1b705cadc877f9f4bc683f5340e775c0f5643
freitzzz <1160907@isep.ipp.pt>
2023-11-26 18:18:13 +0000

    feat: mute and unmute

--- src/autoload/audio_switcher.gd
+++ src/autoload/audio_switcher.gd
@@ -3,9 +3,13 @@ extends Node2D
 var bgm_player: AudioStreamPlayer
 var sfx_player: AudioStreamPlayer
 
+var muted: bool
+
 func _ready():
 	bgm_player = get_node("BGMPlayer")
 	sfx_player = get_node("SFXPlayer")
+
+	muted = false
 
 func switch_bgm(
 	asset: GameAsset
@@ -20,3 +24,18 @@ func play_sfx(
 ):
 	sfx_player.stream = asset.audio()
 	sfx_player.play()
+
+func mute():
+	print("hello")
+	AudioServer.set_bus_mute(AudioServer.get_bus_index(bgm_player.bus), true)
+	AudioServer.set_bus_mute(AudioServer.get_bus_index(sfx_player.bus), true)
+	muted = true
+
+func unmute():
+	print("world")
+	AudioServer.set_bus_mute(AudioServer.get_bus_index(bgm_player.bus), false)
+	AudioServer.set_bus_mute(AudioServer.get_bus_index(sfx_player.bus), false)
+	muted = false
+	
+func is_muted():
+	return muted
